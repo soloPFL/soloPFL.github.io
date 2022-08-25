dbc70c84acecfda90c6cc1b216de0c5ae9be12be
soloPFL <86072748+soloPFL@users.noreply.github.com>
2022-08-25 11:41:00 +0200

    Update install-unbound-hyperlocal-pihole.sh

--- files/install-unbound-hyperlocal-pihole.sh
+++ files/install-unbound-hyperlocal-pihole.sh
@@ -3,7 +3,8 @@
 #Install unbound
 apt install unbound
 #Download the unbound config and creat other configs
-wget -P /etc/unbound/unbound.conf.d/ -O pi-hole.conf https://raw.githubusercontent.com/soloPFL/soloPFL.github.io/main/files/pi-hole.conf-unbound
+wget -O pi-hole.conf https://raw.githubusercontent.com/soloPFL/soloPFL.github.io/main/files/pi-hole.conf-unbound
+mv pi-hole.conf /etc/unbound/unbound.conf.d/pi-hole.conf
 touch /etc/dnsmasq.d/99-edns.conf
 echo 'edns-packet-max=1232' >> /etc/dnsmasq.d/99-edns.conf
 service unbound restart
