f583dbb0c66200d4ab2d1a7d7ee76873c13d6a46
soloPFL <86072748+soloPFL@users.noreply.github.com>
2024-09-09 11:19:31 +0200

    npm host network setting

---
+++ files/ubuntu server docker scripts 2024/npm-installV2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Erstelle den Ordner 'npm' relativ zum aktuellen Verzeichnis des Skripts
+mkdir -p ./npm
+
+# Erstelle die Datei 'docker-compose.yml' im 'npm'-Verzeichnis und füge den Inhalt hinzu
+cat <<EOL > ./npm/docker-compose.yml
+version: '3.8'
+services:
+  app:
+    image: 'jc21/nginx-proxy-manager:latest'
+    restart: unless-stopped
+    network_mode: host
+    volumes:
+      - ./data:/data
+      - ./letsencrypt:/etc/letsencrypt
+
+EOL
+
+# Wechsel in den 'npm'-Ordner
+cd ./npm
+
+# Führe den Befehl 'sudo docker compose up -d' aus
+sudo docker compose up -d
+
+echo "Ordner 'npm' und Datei 'docker-compose.yml' wurden erfolgreich erstellt und NPM wurde gestartet!"
